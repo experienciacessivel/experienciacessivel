5a3dd49b0ffac42878c90a3ed8f4fec8b6da6180
Alexandre Costa <alexandresantoscosta@gmail.com>
2012-06-05 23:54:50 -0300

    Adicionado Entity Framework e a primeira verso do contexto

---
+++ experienciacessivel.infrastructure/DataStoreManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using experienciacessivel.domain;
+
+namespace experienciacessivel.infrastructure
+{
+    public class DataStoreManager : IDataStore
+    {
+        public void CreateStore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteStore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ experienciacessivel.infrastructure/ExperienciacessivelContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using experienciacessivel.domain;
+
+namespace experienciacessivel.infrastructure
+{
+    internal class ExperienciacessivelContext : DbContext
+    {
+        public DbSet<Local> Locais { get; private set; }
+        public DbSet<AccessibleAttributes> AccessibleAttributes { get; set; }
+    }
+}
