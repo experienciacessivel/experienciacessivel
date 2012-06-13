a9d40821b8461273ffc734c7a45dfcfdf92fdc12
rafelton <rafelton@gmail.com>
2012-06-12 23:39:46 -0300

    adicionado mais alguns registros na base

--- experienciacessivel.console/Program.cs
+++ experienciacessivel.console/Program.cs
@@ -19,6 +19,8 @@ namespace experienciacessivel.console
             var a2 = new AccessibleAttributes("Rampas de Acesso");
             var a3 = new AccessibleAttributes("Banheiro Adaptado");
             var a4 = new AccessibleAttributes("Atendimento Especializado");
+            var a5 = new AccessibleAttributes("Espaço para Locomoção");
+            var a6 = new AccessibleAttributes("Escadas");
 
             db.AccessibleAttributes.Add(a1);
             db.AccessibleAttributes.Add(a2);
@@ -39,15 +41,27 @@ namespace experienciacessivel.console
             l2.Add(a3);
             l2.Add(a4);
 
+            l3.Add(a4);
+            l3.Add(a5);
+            l3.Add(a6);
+
+            l4.Add(a3);
+            l4.Add(a4);
+            l4.Add(a5);
+
             var lo1 = new Local("Bar Brahma", l1);
-            var lo2 = new Local("RM pet shop", l1);
+            var lo2 = new Local("RM pet shop", l4);
             var lo3 = new Local("Itaquerão", l2);
             var lo4 = new Local("Aeroporto de Congonhas", l2);
+            var lo5 = new Local("Toldo Preto", l3);
+            var lo6 = new Local("Maçã Verde", l4);
 
             db.Locais.Add(lo1);
             db.Locais.Add(lo2);
             db.Locais.Add(lo3);
             db.Locais.Add(lo4);
+            db.Locais.Add(lo5);
+            db.Locais.Add(lo6);
 
 
             db.SaveChanges();
