3b980062d2ff6a53443d964eb534c9e537979a4c
Alexandre Costa <alexandresantoscosta@gmail.com>
2012-07-09 19:41:13 -0300

    Adicionado Logging e primeira versao do modulo de Infraestrutura

---
+++ experienciacessivel.infrastructure.crosscutting/Logging/ILoggerFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace experienciacessivel.infrastructure.crosscutting.Logging
+{
+    public interface ILoggerFactory
+    {
+        /// <summary>
+        /// Create a new ILog
+        /// </summary>
+        /// <returns>The ILog created</returns>
+        ILogger Create();
+    }
+}
---
+++ experienciacessivel.infrastructure.crosscutting/Logging/LoggerFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace experienciacessivel.infrastructure.crosscutting.Logging
+{
+    public static class LoggerFactory
+    {
+        #region Members
+
+        static ILoggerFactory _currentLogFactory = null;
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Set the  log factory to use
+        /// </summary>
+        /// <param name="logFactory">Log factory to use</param>
+        public static void SetCurrent(ILoggerFactory logFactory)
+        {
+            _currentLogFactory = logFactory;
+        }
+
+        /// <summary>
+        /// Createt a new <paramref name="Microsoft.Samples.NLayerApp.Infrastructure.Crosscutting.Logging.ILog"/>
+        /// </summary>
+        /// <returns>Created ILog</returns>
+        public static ILogger CreateLog()
+        {
+            return (_currentLogFactory != null) ? _currentLogFactory.Create() : null;
+        }
+
+        #endregion
+    }
+}
