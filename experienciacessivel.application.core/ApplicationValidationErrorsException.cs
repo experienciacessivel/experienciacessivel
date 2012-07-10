701a8704e834aaad76ca9a7becb8e981c6022d84
Alexandre Costa <alexandresantoscosta@gmail.com>
2012-07-09 22:52:19 -0300

    Adicionado core da camada de Aplicacao

---
+++ experienciacessivel.application.core/ApplicationValidationErrorsException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace experienciacessivel.application.core
+{
+    public class ApplicationValidationErrorsException
+    : Exception
+    {
+        #region Properties
+
+        IEnumerable<string> _validationErrors;
+        /// <summary>
+        /// Get or set the validation errors messages
+        /// </summary>
+        public IEnumerable<string> ValidationErrors
+        {
+            get
+            {
+                return _validationErrors;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Create new instance of Application validation errors exception
+        /// </summary>
+        /// <param name="validationErrors">The collection of validation errors</param>
+        public ApplicationValidationErrorsException(IEnumerable<string> validationErrors)
+            : base("Validation error")
+        {
+            _validationErrors = validationErrors;
+        }
+
+        #endregion
+    }
+}
---
+++ experienciacessivel.application.core/ProjectionsExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using experienciacessivel.domain;
+using experienciacessivel.infrastructure.crosscutting.Adapter;
+
+namespace experienciacessivel.application.core
+{
+    public static class ProjectionsExtensionMethods
+    {        
+        public static TProjection ProjectedAs<TProjection>(this Entity item)
+            where TProjection : class,new()
+        {
+            var adapter = TypeAdapterFactory.CreateAdapter();
+            return adapter.Adapt<TProjection>(item);
+        }
+        
+        public static List<TProjection> ProjectedAsCollection<TProjection>(this IEnumerable<Entity> items)
+            where TProjection : class,new()
+        {
+            var adapter = TypeAdapterFactory.CreateAdapter();
+            return adapter.Adapt<List<TProjection>>(items);
+        }
+    }
+}
